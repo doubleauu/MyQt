1cd4486449002755abd647e60746fc7fe15550af
ROG <256255779@qq.com>
2026-05-04 19:25:11 +0800

    接入图片资源并对齐恐龙尺寸和跳跃参数

--- MyDino/src/GameWidget.cpp
+++ MyDino/src/GameWidget.cpp
@@ -2,6 +2,15 @@
 
 #include <QPainter>
 #include <QKeyEvent>
+#include <QCoreApplication>  // 应用程序本体的基础功能类
+
+// 匿名命名空间，这些变量只在内部使用，不属于成员变量，不需要暴露，表示规则常量
+// constexpr 表示翻译期常量，强于 const
+namespace {
+constexpr int GroundY = 500;  // 恐龙在地面时左上角 y 坐标
+constexpr int JumpVelocity = 1840;  // 起跳初始速度
+constexpr int Gravity = 80;  // 重力加速度
+}
 
 // 构造函数实现：
 GameWidget::GameWidget(QWidget *parent)
@@ -15,22 +24,27 @@ GameWidget::GameWidget(QWidget *parent)
     // timer 是一个对象，此处要传地址，因为 connect 需要知道哪个对象发出信号
     connect(&timer_, &QTimer::timeout, this, &GameWidget::tick);  // 连接定时器 timer_ 与刷新函数 tick()
     timer_.start(1000/60);  //启动定时器，每隔约 16 毫秒触发一次 timeout 信号，约等于 60 帧率
+
+    // 加载小恐龙图片
+    // Path 函数返回 .exe 文件所在路径，cmake构建会自动将资源文件复制到 exe 旁边
+    const QString imagePath = QCoreApplication::applicationDirPath() + "/Resources/Textures/Run1.png";
+    dinoPixmap_.load(imagePath);
 }
 
 // 界面刷新函数实现
 void GameWidget::tick() {  // 作用域限定符写在返回类型后面
-    const int groundY = 600;  // 恐龙在地面时左上角 y 坐标
-    const int gravity = 2;  // 重力加速度
 
     // 小恐龙跳跃：
     if(inAir_) {  // 如果在空中，就更新跳跃运动
-        dinoRect_.translate(0,velocityY_);  // 在原位置基础上移动恐龙矩形，x 方向不动，y 方向移动 velocityY_（速度）
-        velocityY_ += gravity;  // 速度随重力加速度变化，向上时逐渐变小，向下时逐步变大
+        const int delta = velocityY_ / 60;  // 这一帧恐龙在 Y 轴上要移动多少像素。= 每秒速度/每秒帧数
 
-        if(dinoRect_.y() >= groundY) {  // 当落到地面上时，更新状态，可以进行下一次跳跃；
-            dinoRect_.moveTop(groundY);  // 将矩形顶部强制放回 groundY;
+        if(dinoRect_.y() - delta > GroundY) {  // 当落到地面上时，更新状态，可以进行下一次跳跃；
+            dinoRect_.moveTop(GroundY);  // 将矩形顶部强制放回 groundY;
             velocityY_ = 0;
             inAir_ = false;
+        }else {
+            dinoRect_.translate(0,-delta); // 在原位置基础上移动恐龙矩形，x 方向不动，y 方向移动 velocityY_（速度）
+            velocityY_ -= Gravity;  // 速度随重力加速度变化，向上时逐渐变小，向下时逐步变大
         }
     }
 
@@ -61,17 +75,21 @@ void GameWidget::paintEvent(QPaintEvent *) {
         painter.drawLine(x,700,x+20,720);  // 画一条斜线
     }
 
-    // 简易小恐龙：
-    painter.setBrush(QColor(80,200,120));  // 设置填充颜色
-    painter.setPen(Qt::NoPen);  // 设置无边框
-    painter.drawRect(dinoRect_);  // 绘制矩形
+    // 小恐龙
+    if(!dinoPixmap_.isNull()) {  // 如果图片加载成功了就绘制图片
+        painter.drawPixmap(dinoRect_, dinoPixmap_);
+    }else {  // 未加载成功还绘制简易版矩形代替小恐龙
+        painter.setBrush(QColor(80,200,120));  // 设置填充颜色
+        painter.setPen(Qt::NoPen);  // 设置无边框
+        painter.drawRect(dinoRect_);  // 绘制矩形
+    }
 }
 
 // 键盘接受函数：
 void GameWidget::keyPressEvent(QKeyEvent *event) {
     if(event->key()==Qt::Key_Space || event->key()==Qt::Key_Up || event->key()==Qt::Key_W) {
         if(!inAir_) {
-            velocityY_ = -28;  // 初始速度：-28，后续随重力加速度变化
+            velocityY_ = JumpVelocity;  // 初始速度，后续随重力加速度变化
             inAir_ = true;
         }else {
             QWidget::keyPressEvent(event);
--- MyDino/src/GameWidget.h
+++ MyDino/src/GameWidget.h
@@ -2,6 +2,7 @@
 
 #include <QWidget>
 #include <QTimer>
+#include <QPixmap>  // Qt 中用来保存图片的类
 
 class GameWidget : public QWidget {  // 继承自 QWitget 类
 public:
@@ -14,11 +15,13 @@ private:
     QTimer timer_; // 计时器，负责每隔一段时间提醒程序刷新
     void tick();  // 游戏每一帧要执行的函数，即每一帧的更新入口
 
-    QRect dinoRect_ {100,600,80,100};  // 恐龙的位置和大小
+    QRect dinoRect_ {20,500,200,200};  // 恐龙的位置和大小
     int velocityY_ = 0;  // 竖直方向速度
     bool inAir_ = false; // 是否在空中，防止无限连跳
 
     int groundOffset_ = 0;  // 背景偏移量
     int scrollSpeed_ = 6;  // 背景偏移速度
+
+    QPixmap dinoPixmap_;  // 存放小恐龙图片
 
 };
